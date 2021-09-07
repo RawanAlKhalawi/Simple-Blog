cb9d522663a28da8f428c9fb240a7b115221a522
Rawan AlKhalawi <rawanalkhalawi@gmail.com>
2021-09-07 22:23:15 +0300

    first commit

---
+++ src/test/kotlin/com/rawan/blog/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rawan.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
